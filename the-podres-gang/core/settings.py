0cf122bd2f88d53fe584b77bf08bd5e8435d76be
ZIUGOD <caique.dubstep@gmail.com>
2025-02-18 01:44:55 -0300

    Better path for templates

--- the-podres-gang/core/settings.py
+++ the-podres-gang/core/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
 from django.core.management.utils import get_random_secret_key
+import environ
 
 env = environ.Env()
 environ.Env.read_env()
@@ -62,7 +62,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

bbd39107e710c6f304d23c179622eb5db2f6d56e
ZIUGOD <caique.dubstep@gmail.com>
2025-03-10 01:37:00 -0300

    Views updated

--- the-podres-gang/features/gallery/views.py
+++ the-podres-gang/features/gallery/views.py
@@ -1,51 +1,36 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from features.gallery.models import Image, User
-from django.views.generic import ListView, View
+from features.gallery.models import Image
+from django.views.generic import ListView, TemplateView, DetailView
 
 
 class Home(ListView):
-    # model = Image # same as `queryset = Image.objects.all()`
     queryset = Image.objects.order_by("-pub_date")
+    template_name = "gallery/index.html"
     context_object_name = "images"
-    template_name = (
-        "gallery/index.html"  # mandatory, so dont use the "vanilla" object list
-    )
 
     def get_queryset(self):
         return super().get_queryset()
 
 
-class About(View):
-    GREETINGS = "This is the about page"
-
-    def get(self, request):
-        return HttpResponse(self.GREETINGS)
-
+class About(TemplateView):
+    template_name = "gallery/about.html"
 
 # -x- # -x- # -x- # -x- # -x- # -x- # -x- # -x- #
 
 
-class CreateImage(View):
+class CreateImage(TemplateView):
     GREETINGS = "This is the create image page"
 
     def get(self, request):
         return HttpResponse(self.GREETINGS)
 
 
-class DetailImage(View):
+class DetailImage(DetailView):
     context_object_name = "image"
     model = Image
     template_name = "gallery/detail.html"
+    # success_url = reverse("home")
+    context_object_name = "image"
 
-    def get_object(self, queryset=None):
-        image = super().get_object_or_404(queryset)
-        return image
-    
-
-    # def get_queryset(self, request):
-    #     self.image = get_object_or_404(Image, pk=self.kwargs["image.id"])
-    #     return self.image
-
-    # def get(self, request):
-    #         return HttpResponse(self.GREETINGS)
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)    
